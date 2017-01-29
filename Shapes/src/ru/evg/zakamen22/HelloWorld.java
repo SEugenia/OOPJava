8b7b91ac915ea91514f0d9ca241f4b2933d446df
Евгения <zakamen22@gmail.com>
2017-01-29 19:05:43 +0300

    Добрый вечер!

---
+++ Shapes/src/ru/evg/zakamen22/HelloWorld.java
@@ -0,0 +1,10 @@
+package ru.evg.zakamen22;
+
+/**
+ * Created by Евгения on 29.01.2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+     System.out.println("Hello World!");
+    }
+}
