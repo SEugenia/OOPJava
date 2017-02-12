213f53f0037004504d3c076aa3c5c50f28d33b90
Евгения <zakamen22@gmail.com>
2017-02-12 15:06:56 +0300

    Добрый день. Посмотрите, пожалуйста, логику программы. Верно или нет? Как печатать массив не понимаю.

--- Range/src/ru/evg/zakamen23/range/Range.java
+++ Range/src/ru/evg/zakamen23/range/Range.java
@@ -40,59 +40,218 @@ public class Range {
 
         if (to < range.from) {
             return null;
-        } else if (range.from < from && from > range.to) {
+        } else if (from > range.to) {
             return null;
         } else if (range.from < from && to > range.to) {
-            Range extraRange = new Range(from, to);
-            extraRange.from = from;
-            extraRange.to = range.to;
+            Range extraRange = new Range(from, range.to);
             return extraRange;
         } else if (range.from < from && range.to > to) {
             Range extraRange = new Range(from, to);
             return extraRange;
         } else if (to > range.from && range.to > to) {
-            Range extraRange = new Range(from, to);
-            extraRange.from = range.from;
-           return extraRange;
-        } else if (range.from < to && range.to < to){
-
-            return range;}
-        Range extraRange = new Range(from, to);
-        return extraRange;
-    }
-     /*public Range intersect(Range range) {
-        if (to < range.from) {
-            return null;
-        } else if (range.from == to) {
-            range.to = to;
-            return range;
-        } else if (range.from < from && from > range.to) {
-            return null;
-        } else if (range.from < from && to > range.to) { //(range.from < from && to > range.from && to > range.to)
-            range.from = from; // начало второго интервала меньше первого
-            return range;
-        } else if (range.from < from && range.to > to) {
-            range.from = from;
-            range.to = to;
-            return range;
-        } else if (to > range.from && range.to > to) {
-            range.to = to;
-            return range;
+            Range extraRange = new Range(range.from, to);
+            return extraRange;
+        } else if (range.from < to && range.to < to) {
+            Range extraRange = new Range(range.from, range.to);
+            return extraRange;
         }
+        //Range extraRange = new Range(from, to);
         return range;
-    }*/
+    }
 
-       /* public Range[] union (Range[]array, Range range){
+    public Range[] union(Range range) {
+        if (to < range.from) {
+            Range result1 = new Range(from, to);
+            Range result2 = new Range(range.from, range.to);
+            Range[] array = new Range[2];
+            array[0] = result1;
+            array[1] = result2;
+            System.out.print("объединение ");
+           /* for (int i = 0; i < array.length; ++i) {
+                System.out.print(array[i]);
+            }*/
+            return array;
+        } else if (from > range.to) {
+            Range result1 = new Range(range.from, range.to);
+            Range result2 = new Range(from, to);
+            Range[] array = new Range[2];
+            array[0] = result1;
+            array[1] = result2;
+            System.out.print("объединение ");
+            /*for (int i = 0; i < array.length; ++i) {
+                System.out.print(array[i]);
+            }*/
+            return array;
+        } else if (range.from < from && to > range.to) {
+            Range result = new Range(range.from, to);
+            Range[] array = new Range[1];
+            array[0] = result;
+            System.out.print("объединение ");
+            /*for (int i = 0; i < array.length; ++i) {
+                System.out.print(array[i]);
+            }*/
+            return array;
+        } else if (range.from < from && range.to > to) {
+            Range result = new Range(range.from, range.to);
+            Range[] array = new Range[1];
+            array[0] = result;
+            System.out.print("объединение ");
+            /*for (int i = 0; i < array.length; ++i) {
+                System.out.print(array[i]);
+            }*/
+            return array;
+        } else if (to > range.from && range.to > to) {
+            Range result = new Range(from, range.to);
+            Range[] array = new Range[1];
+            array[0] = result;
+            System.out.print("объединение ");
+            /*for (int i = 0; i < array.length; ++i) {
+                System.out.print(array[i]);
+            }*/
+            return array;
+        } else if (range.from < to && range.to < to) {
+            Range result = new Range(from, to);
+            Range[] array = new Range[1];
+            array[0] = result;
+            System.out.print("объединение ");
+            /*for (int i = 0; i < array.length; ++i) {
+                System.out.print(array[i]);
+            }*/
+            return array;
+        }
 
-            if (range.from > to) {
+        Range[] array = new Range[2];
+        return array;
+    }
 
+    public Range[] difference(Range range) {
+        if (to < range.from) {
+            Range result = new Range(from, to);
+            Range[] array = new Range[1];
+            array[0] = result;
+            System.out.println();
+            System.out.print("разность ");
+            /*for (int i = 0; i < array.length; ++i) {
+                System.out.print(array[i]);
+            }*/
+            return array;
+        } else if (from > range.to) {
+            Range result = new Range(from, to);
+            Range[] array = new Range[1];
+            array[0] = result;
+            System.out.println();
+            System.out.print("разность ");
+            /*for (int i = 0; i < array.length; ++i) {
+                System.out.print(array[i]);
+            }*/
+            return array;
+        } else if (range.from < from && to > range.to) {
+            Range result = new Range(range.to, to);
+            Range[] array = new Range[1];
+            array[0] = result;
+            System.out.println();
+            System.out.print("разность ");
+            /*for (int i = 0; i < array.length; ++i) {
+                System.out.print(array[i]);
+            }*/
+            return array;
+        } else if (range.from < from && range.to > to) {
+            System.out.println();
+            System.out.print("разность null");
+            return null;
+        } else if (to > range.from && range.to > to) {
+            Range result = new Range(from, range.from);
+            Range[] array = new Range[1];
+            array[0] = result;
+            System.out.println();
+            System.out.print("разность ");
+            /*for (int i = 0; i < array.length; ++i) {
+                System.out.print(array[i]);
+            }*/
+            return array;
+        } else if (range.from < to && range.to < to) {
+            Range result1 = new Range(from, range.from);
+            Range result2 = new Range(range.to, to);
+            Range[] array = new Range[2];
+            array[0] = result1;
+            array[1] = result2;
+            System.out.println();
+            System.out.print("разность ");
+            /*for (int i = 0; i < array.length; ++i) {
+                System.out.print(array[i]);
+            }*/
+            return array;
+        }
+        Range[] array = new Range[0];
+        return array;
+    }
 
-            }
-      }
-*/
     public String toString() {
         return "[" + from + " ; " + to + "]";
     }
-
 }
 
+   /* public Range[] union(Range range1, Range range2) {
+        if (range1.to < range2.from) {
+            Range result1 = new Range(range1.from, range1.to);
+            Range result2 = new Range(range2.from, range2.to);
+            Range[] array = new Range[2];
+            array[0] = result1;
+            array[1] = result2;
+            System.out.print("объединение ");
+            for (int i = 0; i < array.length; ++i) {
+                System.out.print(array[i]);
+            }
+            return array;
+        } else if (range1.from > range2.to) {
+            Range result1 = new Range(range2.from, range2.to);
+            Range result2 = new Range(range1.from, range1.to);
+            Range[] array = new Range[2];
+            array[0] = result1;
+            array[1] = result2;
+            System.out.print("объединение ");
+            for (int i = 0; i < array.length; ++i) {
+                System.out.print(array[i]);
+            }
+            return array;
+        } else if (range2.from < range1.from && range1.to > range2.to) {
+            Range result = new Range(range2.from, range1.to);
+            Range[] array = new Range[1];
+            array[0] = result;
+            System.out.print("объединение ");
+            for (int i = 0; i < array.length; ++i) {
+                System.out.print(array[i]);
+            }
+            return array;
+        } else if (range2.from < range1.from && range2.to > range1.to) {
+            Range result = new Range(range2.from, range2.to);
+            Range[] array = new Range[1];
+            array[0] = result;
+            System.out.print("объединение ");
+            for (int i = 0; i < array.length; ++i) {
+                System.out.print(array[i]);
+            }
+            return array;
+        } else if (range1.to > range2.from && range2.to > range1.to) {
+            Range result = new Range(range1.from, range2.to);
+            Range[] array = new Range[1];
+            array[0] = result;
+            System.out.print("объединение ");
+            for (int i = 0; i < array.length; ++i) {
+                System.out.print(array[i]);
+            }
+            return array;
+        } else if (range2.from < range1.to && range2.to < range1.to) {
+            Range result = new Range(range1.from, range1.to);
+            Range[] array = new Range[1];
+            array[0] = result;
+            System.out.print("объединение ");
+            for (int i = 0; i < array.length; ++i) {
+                System.out.print(array[i]);
+            }
+            return array;
+        }
+
+        Range[] array = new Range[2];
+        return array;
+    }*/
--- Range/src/ru/evg/zakamen23/range/UseRange2.java
+++ Range/src/ru/evg/zakamen23/range/UseRange2.java
@@ -1,15 +1,25 @@
 package ru.evg.zakamen23.range;
 
+import java.util.Arrays;
+
 /**
  * Created by Евгения on 05.02.2017.
  */
 public class UseRange2 {
     public static void main(String[] args) {
-        Range range1 = new Range(24, 26);
-        Range range2 = new Range(23, 28);
-        Range[] Arrays = {range1, range2};
-        System.out.println(range1.intersect(range2));
-        //for (int i = 0; i < array.length; ++i) {
-            //System.out.println(Arrays.toString(array));
+        Range range1 = new Range(5, 23);
+        Range range2 = new Range(0, 14);
+        Range[] array = new Range[1];
+        System.out.println("пересечение " + range1.intersect(range2));
+        range1.union(range2);
+        for (Range currentElement: array){
+            }
+       /* for (int i = 0; i < array.length; ++i) {
+            System.out.println(Arrays.toString(array));
+        }*/
+        range1.difference(range2);
+        for (int i = 0; i < array.length; ++i) {
+            System.out.println(Arrays.toString(array));
         }
     }
+}
