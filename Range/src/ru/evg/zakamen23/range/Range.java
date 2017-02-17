4888cf6e877300537bfd014a62ca8d353c5ff521
Евгения <zakamen22@gmail.com>
2017-02-17 18:02:57 +0300

    Добрый вечер!

--- Range/src/ru/evg/zakamen23/range/Range.java
+++ Range/src/ru/evg/zakamen23/range/Range.java
@@ -36,13 +36,12 @@ public class Range {
         return to - from;
     }
 
-    public boolean intersectionCheck(Range range) {
+    public boolean hasNoIntersection(Range range) {
         return (to <= range.from || range.to <= from);
     }
 
     public Range intersect(Range range) {
-        boolean result = intersectionCheck(range);
-        if (result) {
+        if (hasNoIntersection(range)) {
             return null;
         } else {
             return new Range(Math.max(from, range.from), Math.min(to, range.to));
@@ -51,38 +50,25 @@ public class Range {
 
     public Range[] union(Range range) {
         if (to <= range.from) {
-            Range[] array = {new Range(from, to), new Range(range.from, range.to)};
-            return array;
+            return new Range[]{new Range(from, to), new Range(range.from, range.to)};
         } else if (from >= range.to) {
-            Range[] array = {new Range(range.from, range.to), new Range(from, to)};
-            return array;
+            return new Range[]{new Range(range.from, range.to), new Range(from, to)};
         } else {
-            Range[] array = {new Range(Math.min(from, range.from), Math.max(to, range.to))};
-            return array;
+            return new Range[]{new Range(Math.min(from, range.from), Math.max(to, range.to))};
         }
     }
 
     public Range[] difference(Range range) {
-        boolean result = intersectionCheck(range);
-        // if (to < range.from || from > range.to || to == range.from) {
-        if (result) {
-            Range[] array = {new Range(from, to)};
-            return array;
-        } /*else if (from > range.to) {
-            Range[] array = {new Range(from, to)};
-            return array;
-        }*/ else if (range.from < from && to > range.to) {
-            Range[] array = {new Range(range.to, to)};
-            return array;
-        } else if (range.from < from && range.to > to) {
-            Range[] array = {};
-            return array;
-        } else if (to > range.from && range.to > to) {
-            Range[] array = {new Range(from, range.from)};
-            return array;
-        } else {//(range.from < to && range.to < to) {
-            Range[] array = {new Range(from, range.from), new Range(range.to, to)};
-            return array;
+        if (hasNoIntersection(range)) {
+            return new Range[]{new Range(from, to)};
+        } else if (range.from <= from && to >= range.to) {
+            return new Range[]{new Range(range.to, to)};
+        } else if (range.from <= from && range.to >= to) {
+            return new Range[0];
+        } else if (to >= range.from && range.to >= to) {
+            return new Range[]{new Range(from, range.from)};
+        } else {
+            return new Range[]{new Range(from, range.from), new Range(range.to, to)};
         }
     }
 
@@ -99,49 +85,5 @@ public class Range {
 
 
 
- /* public Range intersect2(Range range) {
 
-        if (to < range.from) {
-            return null;
-        } else if (from > range.to) {
-            return null;
-        } else if (range.from < from && to > range.to) {
-            return new Range(from, range.to);
-        } else if (range.from < from && range.to > to) {
-            return new Range(from, to);
-        } else if (to > range.from && range.to > to) {
-            return new Range(range.from, to);
-        } else { //(range.from < to && range.to < to) {
-            return new Range(range.from, range.to);
-        }
-    }*/
 
-    /* public Range[] union(Range range) {
-        if (to < range.from) {
-            Range result1 = new Range(from, to);
-            Range result2 = new Range(range.from, range.to);
-            Range[] array = {result1,result2};
-             return array;
-        } else if (from > range.to) {
-            Range result1 = new Range(range.from, range.to);
-            Range result2 = new Range(from, to);
-            Range[] array = {result1,result2};
-                        return array;
-        } else if (range.from < from && to > range.to) {
-            Range result = new Range(range.from, to);
-            Range[] array = {result};
-            return array;
-        } else if (range.from < from && range.to > to) {
-            Range result = new Range(range.from, range.to);
-            Range[] array = {result};
-            return array;
-        } else if (to > range.from && range.to > to) {
-            Range result = new Range(from, range.to);
-            Range[] array = {result};
-            return array;
-        } else {//(range.from < to && range.to < to) {
-            Range result = new Range(from, to);
-            Range[] array = {result};
-            return array;
-        }
-    }*/
--- Range/src/ru/evg/zakamen23/range/UseRange2.java
+++ Range/src/ru/evg/zakamen23/range/UseRange2.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 public class UseRange2 {
     public static void main(String[] args) {
         Range range1 = new Range(5, 9);
-        Range range2 = new Range(15, 27);
+        Range range2 = new Range(1, 5);
         System.out.println("пересечение " + range1.intersect(range2));
         Range[] array = range1.union(range2);
         System.out.println("объединение " + Arrays.toString(array));
