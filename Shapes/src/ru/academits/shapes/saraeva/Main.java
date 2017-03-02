217271d49d7d69a482a1fb98113314f1f09a0ba3
Евгения <zakamen22@gmail.com>
2017-03-02 20:12:13 +0300

    Без функции нахождения второго по величине периметра и без переопределения методов

--- Shapes/src/ru/academits/shapes/saraeva/Main.java
+++ Shapes/src/ru/academits/shapes/saraeva/Main.java
@@ -1,7 +1,10 @@
 package ru.academits.shapes.saraeva;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+
 /**
- * Created by Евгения on 26.02.2017.
+ * Created by Евгения on 02.03.2017.
  */
 public class Main {
     public static void main(String[] args) {
@@ -10,11 +13,49 @@ public class Main {
         Shape.Rectangle rectangle2 = new Shape.Rectangle(8, 7);
         Shape.Triangle triangle = new Shape.Triangle(0, 0, 0, 3, 4, 0);
         Shape.Circle circle = new Shape.Circle(10);
-        double[] areas = {square.getArea(), circle.getArea(), rectangle.getArea(), rectangle2.getArea(), triangle.getArea()};
-        System.out.println(getMaxSquare(areas));
+        //double[] areas = {square.getArea(), circle.getArea(), rectangle.getArea(), rectangle2.getArea(), triangle.getArea()};
+        //System.out.println(getMaxSquare(areas));
+        ArrayList<Double> areas = new ArrayList<>();
+        areas.add(square.getArea());
+        areas.add(rectangle.getArea());
+        areas.add(rectangle2.getArea());
+        areas.add(triangle.getArea());
+        areas.add(circle.getArea());
+        ArrayList<Double> perimeters = new ArrayList<>();
+        areas.add(square.getPerimeter());
+        areas.add(rectangle.getPerimeter());
+        areas.add(rectangle2.getPerimeter());
+        areas.add(triangle.getPerimeter());
+        areas.add(circle.getPerimeter());
+
+        System.out.println(getMaxSquare(areas, new AreasComparator()));
     }
 
-    public static double getMaxSquare(double[] areas) {
+    public static double getMaxSquare(ArrayList<Double> areas, Comparator<Double> comparator) {
+        for (int i = 0; i < areas.size() - 1; ++i) {
+            //ArrayList<Double> temp = new ArrayList<>(Arrays.asList(areas.get(i + 1)));
+            double temp = areas.get(i + 1);
+            if (comparator.compare(temp, areas.get(i)) > 0) {    //if (comparator.compare(temp > areas[i]) {
+                continue;
+            }
+            int j = i + 1;
+            while (j > 0 && comparator.compare(temp, areas.get(i)) <= 0) {  //temp <= areas[j - 1]
+                //areas.get(j) = areas.get(j - 1);
+                areas.set(j, areas.get(j - 1));
+                --j;
+            }
+            //areas[j] = temp;
+            areas.set(j, temp);
+
+
+        }
+        System.out.println(areas);
+        return areas.get(areas.size() - 1);
+    }
+}
+
+
+ /* public static double getMaxSquare(double[] areas) {
         double temp = 0;
         for (int i = 0; i < areas.length; ++i) {
             if (areas[i] > temp) {
@@ -22,6 +63,15 @@ public class Main {
             }
         }
         return temp;
-    }
-}
+    }*/
+
+  /*if (temp > areas[i]) {
+                continue;
+            }
+            int j = i + 1;
+            while (j > 0 && temp <= areas[j - 1]) {
+                areas[j] = areas[j - 1];
+                --j;
+            }
+            areas[j] = temp;*/
 
