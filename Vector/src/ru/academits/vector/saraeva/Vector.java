c3f42025c750d8360e1d74318c045387e95174bb
Евгения <zakamen22@gmail.com>
2017-03-11 23:21:57 +0300

    Конструкторы

--- Vector/src/ru/academits/vector/saraeva/Vector.java
+++ Vector/src/ru/academits/vector/saraeva/Vector.java
@@ -6,15 +6,40 @@ package ru.academits.vector.saraeva;
 public class Vector {
     private double[] vector;
 
-    /* public Vector (Vector vector)  {
-         Vector vector2 = new Vector();
-     }*/
+    public Vector(int dimension) {                // конструктор а
+        double[] vector = new double[dimension];
+    }
+
+    public Vector(Vector vector) {          // конструктор b
+        double[] arr = new double[]{};
+        Vector vector2 = new Vector(arr);
+        vector2=vector;
+    }
+
     public Vector(double[] vector) {
-        double[] newVector = new double[vector.length];
+        double[] newVector = new double[vector.length]; // конструктор с
         for (int i = 0; i < vector.length; ++i) {
-            this.vector[i] = newVector[i];
+            vector[i] = newVector[i];
         }
     }
+
+    public Vector(int dimension, double[] vector) {     //конструктор d
+        double[] newVector = new double[vector.length];
+        if (vector.length <= newVector.length) {
+            for (int i = 0; i < vector.length; ++i) {
+                vector[i] = newVector[i];
+            }
+        }
+        if (vector.length > newVector.length) {
+            for (int i = 0; i < newVector.length; ++i) {
+                vector[i] = newVector[i];
+            }
+            for (int i = newVector.length; i < vector.length; ++i) {
+                vector[i] = 0;
+            }
+        }
+    }
+
 
    /* public Vector(double[] vector, double[] ) {
 //TODO copyVector
