eab544dd652e760a661bcbbe310643a12d7e5e36
Евгения <zakamen22@gmail.com>
2017-03-24 17:54:14 +0300

    Сложение и вычитание векторов, получение и установка компоненты вектора по индексу

--- Vector/src/ru/academits/vector/saraeva/Main.java
+++ Vector/src/ru/academits/vector/saraeva/Main.java
@@ -1,14 +1,21 @@
 package ru.academits.vector.saraeva;
 
+import java.util.Arrays;
+
 /**
  * Created by Евгения on 11.03.2017.
  */
 public class Main {
     public static void main(String[] args) {
-        double[] arr = {1, 9, 2, 2};
-        double[] arr2 = {1, 4, 0, 11, 6, 1};
-        Vector vector3 = new Vector(arr);
-        Vector vector = new Vector(arr2);
+        double[] arr = {1, 9, 2, 2, 6, 1};
+        double[] arr2 = {1, 9, 2, 2.0001};
+        double[] arr3 = {1, 9, 2, 2.0002};
+        Vector vector = new Vector(arr);
+        Vector vector2 = new Vector(arr2);
+        Vector vector3 = new Vector(arr3);
+        /*System.out.println(vector.hashCode());
+        System.out.println(vector3.hashCode());
+        System.out.println(vector.equals(vector3));*/
         /*System.out.println(vector3.toString());
         System.out.print(vector.toString());
         System.out.println();
@@ -29,7 +36,20 @@ public class Main {
         //System.out.println(vector.toString());
         //vector.rotateVector();
         //System.out.println(vector.toString());
-        vector.setNewElement(-1, 7);
-        System.out.println(vector.toString());
+        //vector.setNewElement(3, 7);
+        //System.out.println(vector.toString());
+        //System.out.println(vector3.equals(vector));
+    }
+
+    public static double[] addUpVectors(Vector vector2, Vector vector3, double[] arr2, double[] arr3) {
+        int resDimension = Math.max(arr2.length, arr3.length);
+        double[] sumArray = new double[resDimension];
+
+        for (int i = 0; i < resDimension; ++i) {
+
+            sumArray[i] = arr2[i] + arr3[i];
+        }
+        return sumArray;
     }
 }
+
